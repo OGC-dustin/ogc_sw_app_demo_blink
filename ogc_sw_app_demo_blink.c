b49521e88a20982e477ff724e6c00a3030866467
dustin <dustin@ogc.engineering>
2024-06-23 04:21:50 -0500

    WIP update filename references

--- ogc_sw_app_demo_blink.c
+++ ogc_sw_app_demo_blink.c
@@ -1,6 +1,6 @@
 /*
  * OGC.Engineering
- * ogc_app_demo_blink.c
+ * ogc_sw_app_demo_blink.c
  * dustin ( at ) ogc.engineering
  *
  * Demonstration application for blinking a status indicator ( binary and multicolor )
@@ -10,23 +10,23 @@
  * [ ] Multi color support
  */
 
-#include "ogc_app_demo_indicator_status.h"
+#include "ogc_sw_app_demo_blink.h"
 
 /* support for using the NRTP OS library */
-#ifdef USE_OGC_OS_NRTPS
-#include "ogc_os_ntrps.h"
+#ifdef USE_OGC_SW_LIB_OS_NRTPS
+#include "ogc_sw_lib_os_ntrps.h"
 
 #endif
 
 /* support for using the OODA OS library */
-#ifdef USE_OGC_OS_OODA
-#include "ogc_os_ooda.h"
+#ifdef USE_OGC_SW_LIB_OS_OODA
+#include "ogc_sw_lib_os_ooda.h"
 
 #endif
 
 /* support for using the State Machine OS library */
-#ifdef USE_OGC_OS_STATE
-#include "ogc_os_state.h"
+#ifdef USE_OGC_SW_LIB_OS_STATE
+#include "ogc_sw_lib_os_state.h"
 
 #endif
 
--- ogc_sw_app_demo_blink.h
+++ ogc_sw_app_demo_blink.h
@@ -1,11 +1,11 @@
 /*
  * OGC.Engineering
- * ogc_app_demo_blink.h
+ * ogc_sw_app_demo_blink.h
  * dustin ( at ) ogc.engineering
  *
  * Demonstration application for blinking a status indicator ( binary and multicolor )
  */
-#ifndef OGC_APP_DEMO_BLINK_H
-#define OGC_APP_DEMO_BLINK_H
+#ifndef OGC_SW_APP_DEMO_BLINK_H
+#define OGC_SW_APP_DEMO_BLINK_H
 
-#endif /* OGC_APP_DEMO_BLINK_H */
+#endif /* OGC_SW_APP_DEMO_BLINK_H */
